f26dc68ecedf4ddff16d695cfa4357003a7b984b
Henriqueporfiriopereira <henrique_porfirio@hotmail.com>
2026-04-27 01:32:36 -0300

    fix indentacao banco

--- app.py
+++ app.py
@@ -118,5 +118,6 @@ def logout():
 
 if __name__ == "__main__":
     with app.app_context():
-    db.drop_all()
-    db.create_all()
+        db.drop_all()
+        db.create_all()
+    app.run(debug=True)
